d1970493999df435cfa1e803a988d5ca3f3f3543
Anton Nesterov <farfaraway@imap.cc>
2024-04-03 17:17:02 +0200

    use next image

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import UploadcareImage from '@uploadcare/nextjs-loader';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <UploadcareImage
+    <Image
       alt="A test image"
-      src="https://ucarecdn.com/051be067-e8ac-4901-a0fc-30d5032a7413/image.jpg"
-      width="400"
-      height="300"
+      src="https://ucarecdn.com/9f95f994-a45a-4b60-a6e3-f8f34b2262e3/image.jpg"
+      width="1000"
+      height="800"
+      sizes="(max-width: 100rem) 100vw, 100rem"
     />
   );
 }
